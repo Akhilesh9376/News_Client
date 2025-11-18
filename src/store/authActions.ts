eaede4e8c63d4c47460018386dd8303c335762e3
Akhilesh Yadav <akhileshyadavkokta.com>
2025-11-18 12:07:58 +0530

    changes fixed

--- src/store/authActions.ts
+++ src/store/authActions.ts
@@ -8,7 +8,7 @@ export const loginUser =
     try {
       dispatch(loginStart());
 
-      const res = await fetch(`${API_BASE_}/api/auth/v1/login`, {
+      const res = await fetch(`${API_BASE_}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
